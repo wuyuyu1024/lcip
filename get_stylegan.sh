5f7881a2fde75257a9b277ac66a2d9766d097bb7
Yu Wang <yuwang@duck.com>
2024-10-21 16:56:37 +0200

    use py script in .sh

--- get_stylegan.sh
+++ get_stylegan.sh
@@ -4,4 +4,4 @@ git apply ../myModification.patch
 cd ..
 mkdir models
 # wget https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan2/versions/1/files/stylegan2-afhqv2-512x512.pkl -O models/stylegan2-afhqv2-512x512.pkl
-python scripts/get_dataset_and_model.py
+python ./scripts/get_dataset_and_model.py
