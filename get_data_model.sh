e310434851f2cd251a3b72a01efcd4c19244f0d3
Yu Wang UU <y.wang6@uu.nl>
2024-10-23 11:50:44 +0200

    small fix

--- get_data_model.sh
+++ get_data_model.sh
@@ -4,4 +4,4 @@ git apply ../scripts/myModification.patch
 cd ..
 mkdir models
 # wget https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan2/versions/1/files/stylegan2-afhqv2-512x512.pkl -O models/stylegan2-afhqv2-512x512.pkl
-python ./scripts/get_dataset_and_model.py
+python3 ./scripts/get_dataset_and_model.py
